7c89e739e50b052738dd4ff5c9414b8ba031b0b4
Gabi <146238675+gabrielafreiberger@users.noreply.github.com>
2025-04-07 08:20:39 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const botoes = document.querySelectorAll( ".botao");
 console.log(botoes);
+
 for (let i = 0; i < botoes.length; i++) {
 console.log(i);
 }
+for(let j=0;j<botoes.length;j++){
+    botoes[j].classList.remove("ativo");
+}
+
+
 botoes[i].onclick = function (){
 botoes[i].classList.add("ativo");
 }
+
